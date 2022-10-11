73a99458b9f73bb24d324b2190bfb1d1bc6cf463
Federico de Mello <Federicodmg20@gmail.com>
2022-10-11 12:27:23 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,68 @@
+let possibleSelections = ["rock", "paper", "scissors"],
+    playerSelection,
+    computerI = "", //Computer Index
+    lastRoundError = false;
+
+function computerPlay() {
+    computerI = Math.floor(Math.random() * 3);
+    return possibleSelections[computerI].toLowerCase();
+}
+
+function playRound() {
+    if (lastRoundError) {
+        playerSelection = prompt("That is not a valid option, please try again!").toLowerCase();
+        lastRoundError = false;
+    } else {
+        playerSelection = prompt("Rock, paper or scissors?").toLowerCase();
+    }
+
+    if (!possibleSelections.includes(playerSelection)) return "That is not a valid option, please try again!";
+    let playerI = possibleSelections.indexOf(playerSelection); //Player Index
+    computerSelection = computerPlay();
+
+    if (playerSelection == computerSelection) return `The game is a tie!`;
+
+    if (computerI == (playerI - 1) || computerI == (possibleSelections.length - 1) && playerI == 0) {
+        return `You win! ${playerSelection} beats ${computerSelection}!`;
+    } else {
+        return `You lose! ${computerSelection} beats ${playerSelection}!`;
+    }
+}
+
+function game() {
+    let playerScore = 0,
+        computerScore = 0,
+        tieScore = 0;
+
+    for (i = 0; i < 5;) {
+        let result = playRound();
+
+        if (result.includes("win")) {
+            i += 1;
+            playerScore += 1;
+            console.log("You won this round!");
+        } else if (result.includes("lose")) {
+            i += 1;
+            computerScore += 1;
+            console.log("You lost this round!");
+        } else if (result.includes("tie")) {
+            i += 1;
+            tieScore += 1;
+            console.log("This round was a tie!");
+        } else {
+            console.log("Try again!");
+            lastRoundError = true;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        alert("You won the game!");
+    } else if (computerScore > playerScore) {
+        alert("You lost the game!");
+    } else if (
+        tieScore > computerScore && tieScore > playerScore ||
+        computerScore == playerScore
+    ) {
+        alert("The game resulted in a tie!");
+    }
+}
